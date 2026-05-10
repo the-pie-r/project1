3171360c142b565e229af5c24d207c6119be6897
pixel-pie-rupam <bwubnc23145@brainwareuniversity.ac.in>
2026-05-10 14:56:22 +0530

    new2

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,11 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace DemoApp.Pages
+namespace project.Pages
 {
     public class IndexModel : PageModel
     {
         public void OnGet()
         {
+
         }
     }
 }
